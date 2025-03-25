0413345fc58a831ed3fc324ece01582b9ec26b46
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2025-03-25 22:52:31 +0100

    Add test for panic in construct of board

--- sudoku_solver/src/sudoku.rs
+++ sudoku_solver/src/sudoku.rs
@@ -9,6 +9,7 @@ impl Sudoku {
         if !Validator::is_valid_board(&board) {
             panic!("Invalid board");
         }
+
         Self { board }
     }
 
@@ -61,6 +62,28 @@ impl Sudoku {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::panic;
+
+    #[test]
+    fn test_invalid_board_panic(){
+        let invalid_board_with_duplicates = vec![
+            vec![5, 3, 5, 6, 7, 0, 0, 0, 0], // Duplicated 5's in the first row
+            vec![6, 0, 0, 1, 9, 5, 0, 0, 0],
+            vec![0, 9, 8, 0, 0, 0, 0, 6, 0],
+            vec![8, 0, 0, 0, 6, 0, 0, 0, 3],
+            vec![4, 0, 0, 8, 0, 3, 0, 0, 1],
+            vec![7, 0, 0, 0, 2, 0, 0, 0, 6],
+            vec![0, 6, 0, 0, 0, 0, 2, 8, 0],
+            vec![0, 0, 0, 4, 1, 9, 0, 0, 5],
+            vec![0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ];
+
+        let result = panic::catch_unwind(|| {
+            Sudoku::new(invalid_board_with_duplicates);
+        });
+
+        assert!(result.is_err(), "Expected panic for invalid board");
+    }
 
     #[test]
     fn test_solve() {
